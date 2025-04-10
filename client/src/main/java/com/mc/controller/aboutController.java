5ebdbf549b98c4fa870d3a9974b037c250a60bb4
Linea89kr <Linea89kr@gmail.com>
2025-04-10 16:05:38 +0900

    [feat] 잘못 올린 java 파일 삭제 및 application.yml 원상복구.

---
+++ client/src/main/java/com/mc/controller/aboutController.java
@@ -0,0 +1,20 @@
+package com.mc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/about")
+public class aboutController {
+
+    String dir = "about/";
+
+    @RequestMapping("")
+    public String about(Model model) {
+        model.addAttribute("header", dir + "header");
+        model.addAttribute("center", dir + "center");
+        model.addAttribute("footer", dir + "footer");
+        return "index";
+    }
+}
