8f9e0fefa14174516e599e1f608ce4ea6833f8d4
kungfugay <paidthatmoney@gmail.com>
2025-04-23 09:53:20 +0900

    [chore] payment 관련 프레임 갖고 오기

---
+++ admin/src/main/java/com/mc/app/repository/PaymentRepository.java
@@ -0,0 +1,17 @@
+package com.mc.app.repository;
+
+import com.mc.app.dto.Payments;
+import com.mc.app.frame.MCRepository;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@Mapper
+public interface PaymentRepository extends MCRepository<Payments,Integer> {
+    Payments selectOneTwo(Payments payments) throws Exception;
+    // 추가: guestId로 모든 결제 내역 조회
+    List<Payments> selectPyAll(Payments payments) throws Exception;
+    Payments selectPayment(Integer integer) throws Exception;
+}
