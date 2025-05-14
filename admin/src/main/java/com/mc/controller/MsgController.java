d736fb9d892272381f2b2b63780510140dce1252
kungfugay <paidthatmoney@gmail.com>
2025-05-14 10:44:56 +0900

    msg 객체에 스페이스 정보(이름) 추가

--- admin/src/main/java/com/mc/controller/MsgController.java
+++ admin/src/main/java/com/mc/controller/MsgController.java
@@ -21,17 +21,17 @@ public class MsgController {
         template.convertAndSend("/sub",msg);
     }
 
-    @MessageMapping("/receiveme") // 나에게만 전송 ex)Ch
+    @MessageMapping("/receiveme") // 나에게만 전송 ex)Chatbot
     public void receiveme(Msg msg, SimpMessageHeaderAccessor headerAccessor) {
         log.info(msg.toString());
 
-        String id = msg.getSendid();
+        String id = msg.getSenderId();
         template.convertAndSend("/sub/"+id,msg);
     }
 
     @MessageMapping("/receiveto") // 특정 Id에게 전송
     public void receiveto(Msg msg, SimpMessageHeaderAccessor headerAccessor) {
-        String target = msg.getReceiveid();
+        String target = msg.getReceiverId();
         log.info("-------------------------");
         log.info(msg.toString());
 
