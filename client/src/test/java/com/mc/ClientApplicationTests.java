d3e23e4c83866fcb8b6b42f2b5036b18b8f7c82b
kungfugay <paidthatmoney@gmail.com>
2025-05-15 10:09:32 +0900

    [수정] 체크인/체크아웃 시간 고정 - mapper의 쿼리 수정 - 테스트 코드 추가

--- client/src/test/java/com/mc/ClientApplicationTests.java
+++ client/src/test/java/com/mc/ClientApplicationTests.java
@@ -1,13 +1,39 @@
 package com.mc;
 
+import com.mc.app.dto.Payments;
+import com.mc.app.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @SpringBootTest
+@Slf4j
 class ClientApplicationTests {
 
+    @Autowired
+    PaymentService paymentService;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception {
+        Payments pay = Payments.builder()
+                .guestId("cf8e3269")
+                .accommodationId(1)
+                .checkIn(LocalDate.parse("2025-05-22"))
+                .checkOut(LocalDate.parse("2025-06-01"))
+                .payAmount(BigDecimal.valueOf(999))
+                .payStatus("취소")
+                .payMeans("카드")
+                .impUid("imp_629828209570")
+                .build();
+
+        log.info(pay.toString());
+        paymentService.add(pay);
     }
 
 }
