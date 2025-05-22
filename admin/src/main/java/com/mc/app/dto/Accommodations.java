70e528da7dd9e52f4b5af4c4c577aabe39859568
kungfugay <paidthatmoney@gmail.com>
2025-05-22 11:41:21 +0900

    #238 Accommodations DTO 조회수, 공지사항 칼럼 추가

--- admin/src/main/java/com/mc/app/dto/Accommodations.java
+++ admin/src/main/java/com/mc/app/dto/Accommodations.java
@@ -19,7 +19,8 @@ public class Accommodations {
     private String location;             // 숙소 주소
     private int priceNight;              // 1박 가격
     private int personMax;               // 최대 수용 인원
-    private String description;          // 상세 설명
+    private String description;          // 상세 설명 (소개글)
+    private String notice;               // 공지사항
     private String status;               // ENUM('활성','비활성')
     private String category;             // ENUM('아파트','단독주택','오피스텔','빌라')
     private String roomType;             // 객실 유형
@@ -41,4 +42,5 @@ public class Accommodations {
     private BigDecimal longitude;        // 경도
     private Timestamp createDay;         // 등록일
     private Timestamp updateDay;         // 수정일
+    private int views;                   // 조회수
 }
